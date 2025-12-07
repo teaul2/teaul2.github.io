abc87146eb7578ef91ad72045dea6911567db3e1
teaul2 <teaul2@gmail.com>
2025-12-08 01:14:35 +0900

    Delete FLASK_Homework directory

--- FLASK_Homework/file.py
+++
@@ -1,8 +0,0 @@
-def save_to_file(file_name, jobs):
-    file = open(f"{file_name}.csv", "w")
-    file.write("Title,Company,Location,Reward,Link\n")
-
-    for job in jobs:
-        file.write(f"{job['title']},{job['company_name']}, {job['location']}, {job['reward']}, {job['link']}\n")
-
-    file.close()
